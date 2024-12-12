554df24fd69455d7b8b1abdd3722824c5dce2355
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-13 01:37:38 +0200

    variable declartion

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -11,6 +11,19 @@
             //Console.WriteLine("Hello, World!");
             //Console.WriteLine("Hello, World!");
             #endregion
+
+            #region variable declartion
+            ////int Number;
+            ////Number =5;
+            ////Console.WriteLine(Number);
+
+            //int Number = 5;
+            //int Number1, Number2, Number3;
+            //int Number4=2,Number5=6,Number7=10;
+            //Console.WriteLine(Number5);
+            //Number4 = 30;
+            //Console.WriteLine(Number4); 
+            #endregion
         }
     }
 }
